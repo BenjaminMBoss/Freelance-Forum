4f9d6bb0e43d486a771138775b1f31bb56b14da7
Benjamin <benjamin.boss@icloud.com>
2023-11-22 23:17:19 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const names = [
+    "Dr. Slice",
+    "Dr. Pressure",
+    "Prof. Possibility",
+    "Prof. Prism",
+    "Dr. Impulse",
+    "Prof. Spark",
+    "Dr. Wire",
+    "Prof. Goose"
+  ];
+  
+const occupations = [
+    "gardener",
+    "programmer",
+    "teacher",
+    "gardner"
+  ];
+  
+const freelancers = [
+    { name: "Dr. Slice", price: 25, occupation: "gardener" },
+    { name: "Dr. Pressure", price: 51, occupation: "programmer" },
+    { name: "Prof. Possibility", price: 43, occupation: "teacher" },
+    { name: "Prof. Prism", price: 81, occupation: "teacher" },
+    { name: "Dr. Impulse", price: 43, occupation: "teacher" },
+    { name: "Prof. Spark", price: 76, occupation: "programmer" },
+    { name: "Dr. Wire", price: 47, occupation: "teacher" },
+    { name: "Prof. Goose", price: 72, occupation: "driver" },
+  ];
+const forumList = [];
+const randFreelancerOne = freelancers[Math.floor(Math.random()*freelancers.length)];
+const randFreelancerTwo = freelancers[Math.floor(Math.random() * freelancers.length)];
+forumList.push(randFreelancerOne, randFreelancerTwo);
+
+const h4 = document.querySelector("h4");
+const avgPriceHeader = document.querySelector("h3");
+
+let sum = 0;
+let listedFreelancers = 0;
+
+function render() {
+    const html = forumList.map((freelancer) => {
+        return `<h4> Name: ${freelancer.name} Occupation: ${freelancer.occupation} Starting Cost: ${freelancer.price} </h4>`;
+    })
+    h4.innerHTML = html.join("")
+}
+
+function startingPriceRender () {
+    const html = forumList.map((freelancer) => {
+        listedFreelancers++;
+        return `${freelancer.price}`
+    })
+    for (let prop in html) {
+        sum += html[prop] * 1;
+    }
+    let avg = sum / listedFreelancers;
+    let str = `<h3> The average starting price is: $${avg} </h3>`;
+    avgPriceHeader.innerHTML += str;
+}
+
+render();
+startingPriceRender();
